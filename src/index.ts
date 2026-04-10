d60290645328842bae5cf1e6638d99fcfa145cc9
Mtc <36608475+Matiascabido@users.noreply.github.com>
2026-04-10 19:11:49 +0000

    chore: update index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import { startDollarJob } from "./jobs/dollar.job";
+import http from "http";
 
-function main() {
-  dotenv.config();
+dotenv.config();
 
-  console.log("🚀 Iniciando dolar cron...");
-  startDollarJob();
-}
+const PORT = process.env.PORT || 3000;
 
-main();
+// 👇 IMPORTANTE: iniciar el job
+startDollarJob();
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("OK");
+}).listen(PORT, () => {
+  console.log(`🌐 Server activo en puerto ${PORT}`);
+});
